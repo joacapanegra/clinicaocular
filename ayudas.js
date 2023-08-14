69d7df586df4a6aaac49342fdc98ec54ade50b09
tupa <jcapanegra@gmail.com>
2023-08-14 19:16:36 -0300

    asfs

--- ayudas.js
+++ ayudas.js
@@ -10,3 +10,14 @@ $(window).on('load resize', function () {
     var carouselHeight = $('.carousel-inner img').height();
     $('.carousel-inner').css('height', carouselHeight);
 });
+
+// Obtén todas las secciones con la clase 'slide-in'
+const slideSections = document.querySelectorAll('.slide-in');
+
+// Función para verificar si una sección está visible en la ventana
+function isSectionVisible(section) {
+  const rect = section.getBoundingClientRect();
+  return (rect.top <= window.innerHeight * 0.8); // Cambia el umbral según tu preferencia
+}
+
+AOS.init();
